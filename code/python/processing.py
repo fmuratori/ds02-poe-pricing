85cb25b2e92ebb0eafc876a2dc593445eb7ba8c7
fmuratori <fabio.muratori2@studio.unibo.it>
2019-04-19 00:39:06 +0200

    Access and minimal parsing of web page content

--- code/python/processing.py
+++ code/python/processing.py
@@ -1,16 +1,34 @@
 import requests
 
-TEST_URL = 'http://www.pathofexile.com/api/public-stash-tabs'
+DEFAULT_URL = 'https://www.pathofexile.com/api/public-stash-tabs?id='
+DEFAULT_NEXT_CHANGE_ID = '348573430-360714130-340379780-391277850-368564540'
+
+def isHealthyStash(stash):
+    return stash['stash'] is not None and stash['stashType'] is not None and stash['league'] is not None    
+
+def parseStashes():
+    nextChangeId = DEFAULT_NEXT_CHANGE_ID
+    
+    newData = []
+
+    for i in range(100):
+        print('Stash - {} - next_change_id: {}'.format(i, nextChangeId))
+
+        url = DEFAULT_URL + nextChangeId        
+        data = requests.get(url).json()
+        nextChangeId = data['next_change_id']
+        for stash in data['stashes']:
+            if isHealthyStash(stash) and 'Synthesis' in stash['league']:
+                newStash = dict()
+                newStash['stashName'] = stash['stash']
+                newStash['stashType'] = stash['stashType']
+                newStash['league'] = stash['league']
+                newData.append(newStash)
+        print(len(newData))
 
 if __name__ == "__main__":
-    data = requests.get(TEST_URL)
-    data = data.json())    
-
-    next_change_id = data['next_change_id']
-    print(next_change_id)
-    print(data.keys())
+    # findOptimalNextChangeId('Synthesis')
+    parseStashes()
 
 
-
-
-
+# 348573430-360714130-340379780-391277850-368564540
