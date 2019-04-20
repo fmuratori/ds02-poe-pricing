bf40e3c377a249a3a2ad132c4c73c2e76dd8268d
fmuratori <fabio.muratori2@studio.unibo.it>
2019-04-20 17:07:03 +0200

    Add automatic web api content download, moved resource folder in data

--- code/python/save_stashes.py
+++ code/python/save_stashes.py
@@ -1,7 +1,9 @@
 import os
 import sys
-p = os.path.abspath('../')
-sys.path.append(p)
+sys.path.append(
+    os.path.abspath(
+        os.path.join(
+            os.path.join(os.path.realpath(__file__), os.pardir), os.pardir)))
 
 import json
 import requests
@@ -9,19 +11,61 @@ import requests
 import constants
 import utilities as utils
 
+nextChangeIdValues = None
 
-with open(constants.LAST_NDI_SAVED_FILE, 'r') as file:
-        nextChangeId = file.read()
 
-i = 1
-while True:
-    print('Loading stash n. {}: {}'.format(i, nextChangeId))
-    i+=1
+def loadNextChangeIdFile():
+    with open(constants.LAST_NDI_SAVED_FILE, 'r') as file:
+        temp = file.readlines()
 
-    nextChangeId, stashes = utils.getStashBatch(constants.DEFAULT_URL + nextChangeId)
-    with open(constants.EXTERNAL_DATA_FOLDER + str(nextChangeId) + '.json', 'w') as file:
-        file.write(json.dumps(stashes))
+    global nextChangeIdValues
+    if len(temp) == 0:
+        nextChangeIdValues = ('0-0-0-0-0', None)
+    elif len(temp) == 1:
+        nextChangeIdValues = (temp[0].strip(), None)
+    elif len(temp) == 2:
+        nextChangeIdValues = (temp[0].strip(), temp[1].strip())
+    else:
+        raise ValueError()
+
+
+def updateNextChangeId(initNextChangeId, finalNextChangeId):
+    global nextChangeIdValues
+    nextChangeIdValues = (initNextChangeId, finalNextChangeId)
 
     with open(constants.LAST_NDI_SAVED_FILE, 'w+') as file:
-        file.write(str(nextChangeId) + '\n')
+        file.write(str(initNextChangeId))
+        if finalNextChangeId is not None:
+            file.write('\n' + str(finalNextChangeId))
 
+
+def getNextChangeIds():
+    return (nextChangeIdValues[0], nextChangeIdValues[1])
+
+
+def saveStashes():
+    loadNextChangeIdFile()
+    saveStash(nextChangeIdValues[0])
+
+
+def saveStash(nextChangeId):
+    # print('Loading stash: {}'.format(nextChangeIdValues[0]))
+    if nextChangeIdValues[1] is not None and int(
+            nextChangeId.split('-')[0]) > int(nextChangeIdValues[1].split('-')[0]):
+        return
+
+
+    newNextChangeId, stashes = utils.getIntegralStashBatch(constants.DEFAULT_URL +
+                                                nextChangeIdValues[0])
+
+    print('SAVE ', constants.ORIGIN_DATA_FOLDER + str(nextChangeId) + '.json')
+    with open(constants.ORIGIN_DATA_FOLDER + str(nextChangeId) + '.json',
+              'w+') as file:
+        file.write(json.dumps(stashes))
+
+    updateNextChangeId(newNextChangeId, nextChangeIdValues[1])
+    saveStash(newNextChangeId)
+
+
+if __name__ == "__main__":
+    saveStashes()
