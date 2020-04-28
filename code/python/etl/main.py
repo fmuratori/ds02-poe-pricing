2e4ed34b479dc300960ad257627247a5ffec0f24
a <a>
2020-04-28 19:20:04 +0200

    Removed empty files and folders

--- code/python/etl/main.py
+++ code/python/etl/main.py
@@ -4,16 +4,16 @@ import configparser
 import logging
 import sys
 
-from extract import FileSystemProvider, APIProvider
-from transform import SimpleTransformer, DBTransformer
-from load import SimpleLoader, DBLoader
+from extract import Provider
+from transform import Transformer
+from load import Loader
+
 
 # logging utility
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 log = logging.getLogger(__name__)
 
 # config variables
-import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -28,11 +28,12 @@ def class_from_string(cstring):
     return getattr(sys.modules[__name__], cstring)
 
 if __name__== '__main__':
-    extract_thread = class_from_string(config['EXTRACT']['EXTRACT_CLASS'])(unpr_list_cond, unpr_list, config)
+    extract_thread = Provider(unpr_list_cond, unpr_list, config)
     extract_thread.start()
 
-    transform_thread = class_from_string(config['TRANSFORM']['TRANSFORM_CLASS'])(unpr_list_cond, pr_list_cond, unpr_list, pr_list, config)
+    transform_thread = Transformer(unpr_list_cond, pr_list_cond,
+                                   unpr_list, pr_list, config)
     transform_thread.start()
 
-    load_thread = class_from_string(config['LOAD']['LOAD_CLASS'])(pr_list_cond, pr_list, config)
+    load_thread = Loader(pr_list_cond, pr_list, config)
     load_thread.start()
