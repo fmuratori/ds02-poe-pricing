59d250f5e3d633b3aa8ce30dc1a627488b34c2ff
a <a>
2020-04-28 19:32:15 +0200

    Revert "Removed empty files and folders"
    This reverts commit 2e4ed34b479dc300960ad257627247a5ffec0f24.

--- code/python/etl/main.py
+++ code/python/etl/main.py
@@ -4,16 +4,16 @@ import configparser
 import logging
 import sys
 
-from extract import Provider
-from transform import Transformer
-from load import Loader
-
+from extract import FileSystemProvider, APIProvider
+from transform import SimpleTransformer, DBTransformer
+from load import SimpleLoader, DBLoader
 
 # logging utility
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 log = logging.getLogger(__name__)
 
 # config variables
+import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -28,12 +28,11 @@ def class_from_string(cstring):
     return getattr(sys.modules[__name__], cstring)
 
 if __name__== '__main__':
-    extract_thread = Provider(unpr_list_cond, unpr_list, config)
+    extract_thread = class_from_string(config['EXTRACT']['EXTRACT_CLASS'])(unpr_list_cond, unpr_list, config)
     extract_thread.start()
 
-    transform_thread = Transformer(unpr_list_cond, pr_list_cond,
-                                   unpr_list, pr_list, config)
+    transform_thread = class_from_string(config['TRANSFORM']['TRANSFORM_CLASS'])(unpr_list_cond, pr_list_cond, unpr_list, pr_list, config)
     transform_thread.start()
 
-    load_thread = Loader(pr_list_cond, pr_list, config)
+    load_thread = class_from_string(config['LOAD']['LOAD_CLASS'])(pr_list_cond, pr_list, config)
     load_thread.start()
