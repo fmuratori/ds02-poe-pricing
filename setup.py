4ccc5347cb48e3687496dcfcfc999c8f6e23e9b0
fmuratori <muratorifabio96@gmail.com>
2020-05-14 09:59:07 +0200

    Updated project setup file and minor changes

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+# "psycopg2==2.8.4",
+
 setup(
     name = "poe-price",
     version = "1.0.0",
@@ -8,11 +10,11 @@ setup(
     include_package_data = True,
     install_requires = [
         "numpy==1.18.1",
+        "psycopg2-binary==2.8.5",
         "configparser==3.7.4",
         "smart-open==1.9.0",
         "pandas==0.25.3",
-        "psycopg2==2.8.4",
-        "scikit-learn==0.22.1",
+        "scikit-learn==0.22.2",
         "click==7.1.2"
     ],
     entry_points = {
