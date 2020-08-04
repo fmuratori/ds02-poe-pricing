a43dfbf31e380aea654b5eece2ef7b194678985d
fmuratori <muratorifabio96@gmail.com>
2020-08-04 11:55:28 +0200

    Added functionalities to data selection from database

--- src/poe_price/data/select.py
+++ src/poe_price/data/select.py
@@ -1,58 +1,82 @@
 from .session import PSQLSession
 
-def _get_by_item_category(category, conn_config):
+
+def _get_by_item_category(categories, conn_config, currency_types):
+    if currency_types is None:
+        currency_types = ['']
+
     data = {}
-    with PSQLSession(conn_config['host'], conn_config['database'], 
+    with PSQLSession(conn_config['host'], conn_config['database'],
                      conn_config['user'], conn_config['password']) as session:
-        data['items'] = session.query('''
-            SELECT * FROM trade_item
-            WHERE category = '{}';
-            '''.format(category))
-        data['items_modifiers'] = session.query('''
+
+        # select items
+        if currency_types is None:
+            data['trade_item'] = session.query('''
+                SELECT * FROM trade_item
+                WHERE category IN ('{}');
+                '''.format('\', \''''.join(categories)))
+        else:
+            data['trade_item'] = session.query('''
+                SELECT * FROM trade_item
+                WHERE category IN ('{}')
+                AND price_currency IN ('{}');
+                '''.format('\', \''''.join(categories), '\', \''''.join(currency_types)))
+
+        # select items additional data
+        data['trade_item_modifier'] = session.query('''
             SELECT tim.* FROM trade_item as ti, trade_item_modifier as tim
             WHERE tim.item_id = ti.id
-            AND ti.category = '{}';
-            '''.format(category))
-        data['items_properties'] = session.query('''
+            AND ti.id IN ({});
+            '''.format(', '.join([str(v) for v in data['trade_item'].id.values])))
+        data['trade_item_property'] = session.query('''
             SELECT tip.* FROM trade_item as ti, trade_item_property as tip
             WHERE tip.item_id = ti.id
-            AND ti.category = '{}';
-            '''.format(category))
-        data['items_sockets'] = session.query('''
+            AND ti.id IN ({});
+            '''.format(', '.join([str(v) for v in data['trade_item'].id.values])))
+        data['trade_item_socket'] = session.query('''
             SELECT tis.* FROM trade_item as ti, trade_item_socket as tis
             WHERE tis.item_id = ti.id
-            AND ti.category = '{}';
-            '''.format(category))
-        data['modifiers'] = session.query('''
-            SELECT tim.* FROM trade_item as ti, trade_item_modifier as tim,
+            AND ti.id IN ({});
+            '''.format(', '.join([str(v) for v in data['trade_item'].id.values])))
+        data['modifier_type'] = session.query('''
+            SELECT mt.* FROM trade_item as ti, trade_item_modifier as tim,
             modifier_type as mt
             WHERE tim.item_id = ti.id
             AND mt.id = tim.modifier_id
-            AND ti.category = '{}';
-            '''.format(category))
-        data['properties'] = session.query('''
-            SELECT tip.* FROM trade_item as ti, trade_item_property as tip,
+            AND ti.id IN ({});
+            '''.format(', '.join([str(v) for v in data['trade_item'].id.values])))
+        data['property_type'] = session.query('''
+            SELECT pt.* FROM trade_item as ti, trade_item_property as tip,
             property_type as pt
             WHERE tip.item_id = ti.id
             AND pt.id = tip.property_id
-            AND ti.category = '{}';
-            '''.format(category))
+            AND ti.id IN ({});
+            '''.format(', '.join([str(v) for v in data['trade_item'].id.values])))
     return data
 
-def get_jewels(conn_config):
-    return _get_by_item_category('jewels', conn_config)
 
-def get_armours(conn_config):
-    return _get_by_item_category('armour', conn_config)
+def get_jewels(conn_config, currency_types=None):
+    return _get_by_item_category(['jewels'], conn_config, currency_types)
 
-def get_weapons(conn_config):
-    return _get_by_item_category('weapons', conn_config)
 
-def get_accessories(conn_config):
-    return _get_by_item_category('accessories', conn_config)
+def get_armours(conn_config, currency_types=None):
+    return _get_by_item_category(['armour'], conn_config, currency_types)
+
+
+def get_weapons(conn_config, currency_types=None):
+    return _get_by_item_category(['weapons'], conn_config, currency_types)
+
+
+def get_accessories(conn_config, currency_types=None):
+    return _get_by_item_category(['accessories'], conn_config, currency_types)
+
+
+def get_mod_items(conn_config, currency_types=None):
+    return _get_by_item_category(['jewels', 'armour', 'weapons', 'accessories'], conn_config, currency_types)
+
 
 def get_currency(conn_config):
-    with PSQLSession(conn_config['host'], conn_config['database'], 
+    with PSQLSession(conn_config['host'], conn_config['database'],
                      conn_config['user'], conn_config['password']) as session:
         data = session.query('SELECT * FROM trade_currency')
     return data
