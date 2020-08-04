2b2f89ccc5c6164e869ee49f3df1beb83de06fec
fmuratori <muratorifabio96@gmail.com>
2020-08-04 11:53:08 +0200

    Minor changes to etl data transformantions

--- src/poe_price/data/etl/transformer.py
+++ src/poe_price/data/etl/transformer.py
@@ -11,27 +11,28 @@ TEMP = []
 
 LEAGUES = ['Harvest']
 CURRENCY_DICT = {
-    'Chromatic Orb': ['chrom'], 
-    'Orb of Alteration': ['alt'], 
-    'Jeweller\'s Orb': ['jewel'], 
-    'Orb of Chance': ['chance'], 
-    'Cartographer\'s Chisel': ['chisel', 'cartographer'], 
-    'Orb of Fusing': ['fuse', 'fusing'], 
-    'Orb of Alchemy': ['alch'], 
+    'Chromatic Orb': ['chrom'],
+    'Orb of Alteration': ['alt'],
+    'Jeweller\'s Orb': ['jewel'],
+    'Orb of Chance': ['chance'],
+    'Cartographer\'s Chisel': ['chisel', 'cartographer'],
+    'Orb of Fusing': ['fusing', 'fuse'],
+    'Orb of Alchemy': ['alch'],
     'Orb of Scouring': ['scour'],
-    'Blessed Orb': ['blessed'], 
-    'Chaos Orb': ['chaos'], 
-    'Orb of Regret': ['regret'], 
-    'Regal Orb': ['regal'], 
-    'Gemcutter\'s Prism': ['gcp', 'gemcutter'], 
-    'Divine Orb': ['divine'], 
-    'Exalted Orb': ['exalted', 'exa'], 
+    'Blessed Orb': ['blessed'],
+    'Chaos Orb': ['chaos'],
+    'Orb of Regret': ['regret'],
+    'Regal Orb': ['regal'],
+    'Gemcutter\'s Prism': ['gcp', 'gemcutter'],
+    'Divine Orb': ['divine'],
+    'Exalted Orb': ['exalted', 'exa'],
     'Mirror of Kalandra': ['mirror']}
-INV_CURRENCY_DICT = {value: k for k, values in CURRENCY_DICT.items() for value in values }
+INV_CURRENCY_DICT = {value: k for k, values in CURRENCY_DICT.items()
+                     for value in values}
 
 ITEM_CATEGORY = ['accessories', 'armour', 'jewels', 'weapons', 'currency']
 
-FRAME_TYPES = {0: 'normal', 1: 'magic', 2:'rare', 3: 'unique', 4: 'gem', 5: 'currency',
+FRAME_TYPES = {0: 'normal', 1: 'magic', 2: 'rare', 3: 'unique', 4: 'gem', 5: 'currency',
                6: 'divination card', 7: 'quest item', 8: 'prophecy', 9: 'relic'}
 
 CURRENCY_COLUMNS = ['league', 'price_currency', 'price_quantity', 'is_stash_price', 'sell_quantity',
@@ -44,6 +45,7 @@ MITEMS_COLUMNS = ['category', 'corrupted', 'duplicated', 'identified', 'ilvl',
                   'requirement_dex', 'requirement_int', 'requirement_level',
                   'requirement_str', 'sub_category', 'synthesised', 'talisman_tier',
                   'date']
+
 
 def get_items(data, df=True):
     items = []
@@ -58,11 +60,14 @@ def get_items(data, df=True):
     return items
 
 # extract new data from existing data
+
+
 def extract_item_category(items):
     items['sub_category'] = items.extended.apply(lambda y: ' '.join(y['subcategories'])
                                                  if 'subcategories' in y else None)
     items['category'] = items.extended.apply(lambda y: y['category'])
     return items
+
 
 def extract_item_price(items, use_stash_price=False):
     prices = []
@@ -74,9 +79,11 @@ def extract_item_price(items, use_stash_price=False):
         if price is (None, None, None, False) and use_stash_price:
             price = (*extract_string_price(v.stash_note), True)
         prices.append(price)
-        
-    (items['price_currency'], items['price_quantity'], items['sell_quantity'], items['is_stash_price']) = zip(*prices)
+
+    (items['price_currency'], items['price_quantity'],
+     items['sell_quantity'], items['is_stash_price']) = zip(*prices)
     return items
+
 
 def extract_string_price(string):
     try:
@@ -90,7 +97,7 @@ def extract_string_price(string):
             else:
                 quantity = round(float(quantity), 2)
                 sellingStackSize = 1
-            
+
             return currency, quantity, sellingStackSize
     except TypeError:
         # no match found
@@ -98,9 +105,12 @@ def extract_string_price(string):
     return (None, None, None)
 
 # filter out unwanted rows
+
+
 def filter_item_category(items, categories):
     if 'jewels' in categories:
-        items = items[~((items.category=='jewels') & (items.sub_category=='cluster'))]
+        items = items[~((items.category == 'jewels') &
+                        (items.sub_category == 'cluster'))]
 
     if 'currency' in categories:
         items = items[~((items.category == 'currency') & (
@@ -108,19 +118,31 @@ def filter_item_category(items, categories):
 
     return items[items.category.isin(categories)]
 
+
 def filter_item_league(items, leagues):
     return items[items.league.isin(leagues)]
 
-def filter_item_price(items):
-    items['price_currency'] = items.price_currency.apply(lambda y: y.lower() if y is not None else None)
 
-    items = items[(items.price_currency.notna()) & 
-                 (items.price_quantity.notna()) & 
-                 (items.sell_quantity.notna()) & 
-                 (items.price_currency.isin(INV_CURRENCY_DICT)) &
-                 (items.price_quantity > 0) &
-                 (items.sell_quantity > 0)]
+def filter_item_price(items):
+    items['price_currency'] = items.price_currency.apply(
+        lambda y: y.lower() if y is not None else None)
+
+    items = items[(items.price_currency.notna()) &
+                  (items.price_quantity.notna()) &
+                  (items.sell_quantity.notna()) &
+                  (items.price_currency.isin(INV_CURRENCY_DICT)) &
+                  (items.price_quantity > 0) &
+                  (items.sell_quantity > 0)]
+
+    # UNTESTED
+    # convert rarely used currency identifier to preferable identifier
+    target_condition = items[items.price_currency.isin(
+        ['cartographer', 'fuse', 'gemcutter', 'exa'])]
+    items[target_condition].price_currency = items[target_condition].price_currency.apply(
+        lambda y: CURRENCY_DICT[INV_CURRENCY_DICT[y]][0])
+
     return items
+
 
 def mitems_prop_formatting(mitems, target_props):
     mitems_props_vocabulary = []
@@ -135,20 +157,24 @@ def mitems_prop_formatting(mitems, target_props):
                     item_prop['item_id'] = k
                     generic_prop = prop['name']
                     try:
-                        prop_index = mitems_props_vocabulary.index(generic_prop)
+                        prop_index = mitems_props_vocabulary.index(
+                            generic_prop)
                     except:
                         mitems_props_vocabulary.append(generic_prop)
                         prop_index = len(mitems_props_vocabulary) - 1
                     item_prop['property_id'] = prop_index
                     for i, v in enumerate(prop['values'][0][0].split('-')):
                         try:
-                            item_prop['value{}'.format(i)] = float(re.sub('(\+|-|%)', '', v))
+                            item_prop['value{}'.format(i)] = float(
+                                re.sub('(\+|-|%)', '', v))
                         except:
                             break
                     mitems_props.append(item_prop)
     mitems_props = pd.DataFrame(mitems_props)
-    mitems_props_vocabulary = pd.DataFrame(mitems_props_vocabulary, columns=['text'])
+    mitems_props_vocabulary = pd.DataFrame(
+        mitems_props_vocabulary, columns=['text'])
     return mitems_props, mitems_props_vocabulary
+
 
 def mitems_mod_formatting(mitems, mod_types):
     mitems_mods_vocabulary = []
@@ -161,7 +187,8 @@ def mitems_mod_formatting(mitems, mod_types):
                     item_mod['item_id'] = k
                     generic_mod = re.sub('\d+', '#', mod)
                     try:
-                        mod_index = mitems_mods_vocabulary.index((generic_mod, mod_type))
+                        mod_index = mitems_mods_vocabulary.index(
+                            (generic_mod, mod_type))
                     except:
                         mitems_mods_vocabulary.append((generic_mod, mod_type))
                         mod_index = len(mitems_mods_vocabulary) - 1
@@ -170,10 +197,12 @@ def mitems_mod_formatting(mitems, mod_types):
                         item_mod['value{}'.format(i)] = int(v)
                     mitems_mods.append(item_mod)
     mitems_mods = pd.DataFrame(mitems_mods)
-    mitems_mods_vocabulary = pd.DataFrame(mitems_mods_vocabulary, columns=['text', 'type'])
+    mitems_mods_vocabulary = pd.DataFrame(
+        mitems_mods_vocabulary, columns=['text', 'type'])
     return mitems_mods, mitems_mods_vocabulary
 
 ############################### HIGH LEVEL TRANSFORMATION ###############################
+
 
 def filter_json(content):
     for i, stash in enumerate(content['stashes']):
@@ -181,12 +210,13 @@ def filter_json(content):
             del(content['stashes'][i])
     return content
 
+
 def extract_items(content):
     dtime = datetime.strptime(content['datetime'], '%m/%d/%Y')
-        
+
     items = get_items(content)
     items['date'] = dtime.strftime("%m/%d/%Y")
-    
+
     items = filter_item_league(items, LEAGUES)
     items = extract_item_category(items)
 
@@ -195,20 +225,23 @@ def extract_items(content):
     items = filter_item_price(items)
     return items
 
+
 def extract_currencies(items):
-    currency = items[items.category=='currency'].copy()
+    currency = items[items.category == 'currency'].copy()
     currency['sell_currency'] = currency.extended.apply(lambda y:
-            CURRENCY_DICT[y['baseType']][0]
-            if y['baseType'] in CURRENCY_DICT else None)
+                                                        CURRENCY_DICT[y['baseType']][0]
+                                                        if y['baseType'] in CURRENCY_DICT else None)
     currency = currency[currency.sell_currency.notna()]
-    currency.drop(columns = [v for v in currency.columns
-                                    if v not in CURRENCY_COLUMNS],
-                             inplace=True, errors='ignore')
+    currency.drop(columns=[v for v in currency.columns
+                           if v not in CURRENCY_COLUMNS],
+                  inplace=True, errors='ignore')
     return currency
 
+
 def extract_mod_items(items):
-    mitems = items[items.category.isin(['jewels', 'armour', 'weapons', 'accessories'])].copy()
-    
+    mitems = items[items.category.isin(
+        ['jewels', 'armour', 'weapons', 'accessories'])].copy()
+
     if len(mitems) > 0:
         mitems_sockets = None
         mitems_mods, mitems_mods_voc = None, None
@@ -217,51 +250,59 @@ def extract_mod_items(items):
         if 'frameType' in mitems:
             mitems['rarity'] = mitems.frameType.apply(lambda y: FRAME_TYPES[y])
             mitems = mitems[(mitems.rarity.isin(['normal', 'magic', 'rare']))]
-        
+
         if 'extended' in mitems:
             mitems['num_prefixes'] = mitems.extended.apply(lambda y: y['prefixes']
-                                                        if 'prefixes' in y else None)
+                                                           if 'prefixes' in y else None)
             mitems['num_suffixes'] = mitems.extended.apply(lambda y: y['suffixes']
-                                                        if 'suffixes' in y else None)
-        
+                                                           if 'suffixes' in y else None)
+
         if 'influences' in mitems:
-            mitems_influences = mitems['influences'].apply(lambda y: y if isinstance(y, dict) else {})
+            mitems_influences = mitems['influences'].apply(
+                lambda y: y if isinstance(y, dict) else {})
             mitems_influences = pd.DataFrame(mitems_influences.to_list(),
                                              index=mitems_influences.index)
             mitems_influences.columns = ['influence_{}'.format(v)
                                          for v in mitems_influences.columns]
-            
-            mitems = pd.merge(mitems, mitems_influences, left_index=True, right_index=True, how='left')
-        
+
+            mitems = pd.merge(mitems, mitems_influences,
+                              left_index=True, right_index=True, how='left')
+
         if 'properties' in mitems:
-            mitems_prop, mitems_prop_voc = mitems_prop_formatting(mitems, ['properties'])
+            mitems_prop, mitems_prop_voc = mitems_prop_formatting(mitems, [
+                                                                  'properties'])
 
         if any([v in mitems for v in ['craftedMods', 'enchantMods',
                                       'explicitMods', 'implicitMods']]):
             mitems_mods, mitems_mods_voc = mitems_mod_formatting(mitems,
                                                                  ['craftedMods', 'enchantMods',
                                                                   'explicitMods', 'implicitMods'])
-        
+
         if 'requirements' in mitems:
             mitems_req = mitems['requirements'].apply(lambda y: {req['name']: int(req['values'][0][0])
                                                                  for req in y}
-                                                                if isinstance(y, list) else {})
-            mitems_req = pd.DataFrame(mitems_req.to_list(), index=mitems_req.index)
+                                                      if isinstance(y, list) else {})
+            mitems_req = pd.DataFrame(
+                mitems_req.to_list(), index=mitems_req.index)
             mitems_req.columns = ['requirement_{}'.format(col_name.lower())
                                   for col_name in mitems_req.columns]
             if 'requirement_strength' in mitems_req:
                 mitems_req.loc[mitems_req.requirement_strength.notna(), 'requirement_str'] = \
-                    mitems_req[mitems_req.requirement_strength.notna()].requirement_strength.values
+                    mitems_req[mitems_req.requirement_strength.notna()
+                               ].requirement_strength.values
             if 'requirement_dexterity' in mitems_req:
                 mitems_req.loc[mitems_req.requirement_dexterity.notna(), 'requirement_dex'] = \
-                    mitems_req[mitems_req.requirement_dexterity.notna()].requirement_dexterity.values
+                    mitems_req[mitems_req.requirement_dexterity.notna(
+                    )].requirement_dexterity.values
             if 'requirement_intelligence' in mitems_req:
                 mitems_req.loc[mitems_req.requirement_intelligence.notna(), 'requirement_int'] = \
-                    mitems_req[mitems_req.requirement_intelligence.notna()].requirement_intelligence.values
+                    mitems_req[mitems_req.requirement_intelligence.notna(
+                    )].requirement_intelligence.values
             mitems_req.drop(columns=['requirement_strength', 'requirement_dexterity',
                                      'requirement_intelligence'], inplace=True, errors='ignore')
-            mitems = pd.merge(mitems, mitems_req, left_index=True, right_index=True, how='left')
-        
+            mitems = pd.merge(mitems, mitems_req,
+                              left_index=True, right_index=True, how='left')
+
         # split mitems socket column into separate dataframe
         if 'sockets' in mitems:
             for k, v in mitems.iterrows():
@@ -274,19 +315,21 @@ def extract_mod_items(items):
                                            for socket in sockets])
             mitems_sockets['colour'] = mitems_sockets['sColour']
             mitems_sockets['socket_group'] = mitems_sockets['group']
-            mitems_sockets.drop(columns=['attr', 'sColour', 'group'], inplace=True)
-        
+            mitems_sockets.drop(
+                columns=['attr', 'sColour', 'group'], inplace=True)
+
         if 'veiledMods' in mitems:
-            mitems['num_veiled_modifiers'] = mitems.veiledMods.apply(lambda y: len(y) if isinstance(y, list) else 0)
-        
+            mitems['num_veiled_modifiers'] = mitems.veiledMods.apply(
+                lambda y: len(y) if isinstance(y, list) else 0)
+
         if 'talismanTier' in mitems:
             mitems['talisman_tier'] = mitems['talismanTier']
             del(mitems['talismanTier'])
-        
-        mitems.drop(columns = [v for v in mitems.columns
-                                    if v not in MITEMS_COLUMNS],
-                             inplace=True, errors='ignore')
 
-        return mitems, mitems_sockets, mitems_prop, mitems_prop_voc, mitems_mods, mitems_mods_voc
+        mitems.drop(columns=[v for v in mitems.columns
+                             if v not in MITEMS_COLUMNS],
+                    inplace=True, errors='ignore')
+
+    return mitems, mitems_sockets, mitems_prop, mitems_prop_voc, mitems_mods, mitems_mods_voc
     else:
         return None, None, None, None, None, None
