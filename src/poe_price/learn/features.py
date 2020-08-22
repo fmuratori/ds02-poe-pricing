ffa139e4e1d38bba3ea415a750cf8bc7e51d5704
fmuratori <muratorifabio96@gmail.com>
2020-08-22 11:08:34 +0200

    Removed redundant files, improved Price feature fit speed

--- src/poe_price/learn/features.py
+++ src/poe_price/learn/features.py
@@ -1,7 +1,7 @@
 from scipy.sparse import csr_matrix
 import numpy as np
 import pandas as pd
-from scipy.sparse import lil_matrix, csr_matrix
+from scipy.sparse import lil_matrix, csr_matrix, vstack
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler
 from abc import ABC, abstractmethod
@@ -43,25 +43,25 @@ class IsAbyssJewel(SimpleEstimator):
 class IsSynthesised(SimpleEstimator):
     def _extract(self, X, y=None):
         feat = X['trade_item'].synthesised.fillna(False)
-        return feat.astype(int).values.reshape(-1, 1)
+        return (feat == True).astype(int).values.reshape(-1, 1)
 
 
 class IsCorrupted(SimpleEstimator):
     def _extract(self, X, y=None):
         feat = X['trade_item']['corrupted'].fillna(False)
-        return feat.astype(int).values.reshape(-1, 1)
+        return (feat == True).astype(int).values.reshape(-1, 1)
 
 
 class IsDuplicated(SimpleEstimator):
     def _extract(self, X, y=None):
         feat = X['trade_item']['duplicated'].fillna(False)
-        return feat.astype(int).values.reshape(-1, 1)
+        return (feat == True).astype(int).values.reshape(-1, 1)
 
 
 class IsIdentified(SimpleEstimator):
     def _extract(self, X, y=None):
         feat = X['trade_item'].identified.fillna(False)
-        return feat.astype(int).values.reshape(-1, 1)
+        return (feat == True).astype(int).values.reshape(-1, 1)
 
 
 class IsVeiled(SimpleEstimator):
@@ -152,7 +152,7 @@ class Requirements(SimpleEstimator):
 class Influences(SimpleEstimator):
     def _extract(self, X, y=None):
         feat = X['trade_item'][INFLUENCES_COLUMNS].fillna(False)
-        return csr_matrix(feat.astype(int).values)
+        return csr_matrix((feat == True).astype(int).values)
 
 
 class Rarity(SimpleEstimator):
@@ -207,8 +207,9 @@ class SocketColour(BaseEstimator):
 
 
 class Modifiers(BaseEstimator):
-    def __init__(self, how='flag'):
+    def __init__(self, how='flag', chunk_size=100000):
         self.how = how
+        self.chunk_size = chunk_size
 
     def fit(self, X, y=None):
         if self.how not in ['flag', 'mean', 'all']:
@@ -218,46 +219,65 @@ class Modifiers(BaseEstimator):
         if self.how == 'all':
             self._modifiers = ['mod{}_value{}'.format(v['id'], i)
                                for _, v in X['modifier_type'].iterrows()
-                               for i in range(v['text'].count('#'))]
+                               for i in range(v['text'].count('#') if '#' in v['text'] else 1)]
         else:
             self._modifiers = X['modifier_type'].id.apply(
                 lambda y: 'mod{}'.format(y)).values
-
         return self
 
     def transform(self, X, y=None):
         # compute unnormalized features
         t = X['trade_item_modifier'].copy()
+        t['value_agg'] = t[[colname for colname in t.columns if 'value' in colname]].mean(
+            axis=1, skipna=True)
         if self.how == 'mean':
-            t['value_agg'] = t[[colname for colname in t.columns if 'value' in colname]].mean(
-                axis=1, skipna=True)
             t['value_agg'].fillna(1, inplace=True)
         elif self.how == 'flag':
             t['value_agg'] = 1
-        if self.how == 'all':
-            t = pd.pivot_table(
-                t, values=['value0', 'value1', 'value2'], index='item_id', columns='modifier_id')
-            t.columns = ['mod{}_{}'.format(b, a) for a, b in t.columns]
-        elif self.how in ['flag', 'mean']:
-            t = pd.pivot_table(t, values='value_agg',
-                               index='item_id', columns='modifier_id')
-            t.columns = ['mod{}'.format(a) for a in t.columns]
-        # fill Dataframe with empty colums representing unseen modifiers
-        for colname in self._modifiers:
-            if colname not in t.columns:
-                t[colname] = 0
-        # order columns following lookup table
-        t = t[self._modifiers]
+        elif self.how == 'all':
+            # fill items modifiers with no value to 1
+            t.loc[t.value_agg.isna(), 'value0'] = 1
 
-        t.fillna(0, inplace=True)
+        rows_iids = []
+        features = []
+        for chunk in self._split(t, self.chunk_size):
+            if self.how in ['mean', 'flag']:
+                chunk = pd.pivot_table(chunk, values='value_agg',
+                                       index='item_id', columns='modifier_id')
+            else:
+                chunk = pd.pivot_table(chunk, values=['value0', 'value1', 'value2'],
+                                       index='item_id', columns='modifier_id')
+            chunk.columns = ['mod{}'.format(a) for a in chunk.columns]
+            # fill Dataframe with empty colums representing unseen modifiers
+            for colname in self._modifiers:
+                if colname not in chunk.columns:
+                    chunk[colname] = 0
+            chunk.fillna(0, inplace=True)
+            # order columns following lookup table
+            features.append(csr_matrix(chunk[self._modifiers].values))
+            rows_iids.extend(chunk.index.values)
 
         # create empty rows associated to items with no modifier
-        missing_items_ids = set(
-            X['trade_item'].id.values) - set(t.index.values)
-        t = t.append(pd.DataFrame(
-            [{} for i in range(len(missing_items_ids))], index=missing_items_ids))
-        t.fillna(0, inplace=True)
-        return csr_matrix(t.loc[X['trade_item'].id].values)
+        missing_items_ids = set(X['trade_item'].id.values) - set(rows_iids)
+        rows_iids.extend(missing_items_ids)
+
+        features.append(csr_matrix(
+            (len(missing_items_ids), len(self._modifiers))))
+        features = vstack(features)
+
+        return features[np.argsort(rows_iids), :]
+
+    def _split(self, df, chunk_size):
+        chunks = [df[i:i+chunk_size].copy()
+                  for i in range(0, len(set(df.item_id.values)), chunk_size)]
+        for i, chunk in enumerate(chunks):
+            if i < len(chunks) - 1:
+                while chunks[i].tail(1).item_id.values[0] == chunks[i+1].head(1).item_id.values[0]:
+                    # move df row between chunks
+                    chunks[i].append(chunks[1].head(1))
+                    chunks[i+1].drop(index=chunks[i +
+                                                  1].head(1).index.values, inplace=True)
+        return chunks
 
 
 class Properties(BaseEstimator):
@@ -338,10 +358,10 @@ class Properties(BaseEstimator):
 
 
 class Price(BaseException):
-    def __init__(self, market_head=10, outlier_window=3):
+    def __init__(self, market_head=10, outlier_window=3, max_price_value=100000):
         '''
         - outlier_window: int
-            Remove trades rate which value is outside the normal distribution window ù
+            Remove trades rate which value is outside the normal distribution window ?
             defined as (mean_value +- standard deviation * window).
 
         - market_head: int
@@ -350,6 +370,7 @@ class Price(BaseException):
         '''
         self.market_head = market_head
         self.outlier_window = outlier_window
+        self.max_price_value = max_price_value
 
     def fit(self, X, y=None):
         data, currencies = X
@@ -365,10 +386,11 @@ class Price(BaseException):
         feat = np.zeros((len(data['trade_item']), 1))
         for i, (k, v) in enumerate(data['trade_item'].iterrows()):
             if v.price_currency != 'chaos':
-                feat[i, 0] = self.c_rates.loc['chaos',
-                                              v.price_currency] * v.price_quantity
+                value = self.c_rates.loc['chaos',
+                                         v.price_currency] * v.price_quantity
             else:
-                feat[i, 0] = v.price_quantity
+                value = v.price_quantity
+            feat[i, 0] = value if value <= self.max_price_value else self.max_price_value
         return feat
 
     def _extract_exchange_rates(self, currencies):
@@ -378,17 +400,16 @@ class Price(BaseException):
             currencies.price_quantity
 
         for v1 in self.currency_set:
-            for v2 in set(self.currency_set):
-                if v1 == v2:
-                    value = 1
-                else:
-                    temp = currencies[(currencies.sell_currency == v1) &
-                                      (currencies.price_currency == v2)].copy()
-                    temp.sort_values('rate', ascending=False, inplace=True)
-                    temp['n_rate'] = temp.rate.apply(lambda y: (
-                        y - temp.rate.mean()) / temp.rate.std())
-                    temp = temp[(temp.n_rate > -self.outlier_window) &
-                                (temp.n_rate < self.outlier_window)].rate
-                    value = round(np.mean(temp.head(self.market_head)), 3)
-
-                self.c_rates.loc[v1, v2] = value
+            v2 == 'chaos'
+            if v1 == v2:
+                value = 1
+            else:  # we are interested only in converting over currencies to the "chaos" type
+                temp = currencies[(currencies.sell_currency == v1) &
+                                  (currencies.price_currency == v2)].copy()
+                temp.sort_values('rate', ascending=False, inplace=True)
+                temp['n_rate'] = temp.rate.apply(lambda y: (
+                    y - temp.rate.mean()) / temp.rate.std())
+                temp = temp[(temp.n_rate > -self.outlier_window) &
+                            (temp.n_rate < self.outlier_window)].rate
+                value = round(np.mean(temp.head(self.market_head)), 3)
+            self.c_rates.loc[v1, v2] = value
